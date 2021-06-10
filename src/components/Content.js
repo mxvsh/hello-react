641ffc5757930a2afc3d5a6a7ad58f58b642152f
xencodes <xencodes@icloud.com>
2021-06-10 07:28:58 +0530

    first part

---
+++ src/components/Content.js
@@ -0,0 +1,11 @@
+function Content() {
+  return (
+    <div>
+      Deserunt mollit eiusmod do consectetur. Aliquip reprehenderit commodo
+      magna amet voluptate cupidatat. Ullamco et Lorem commodo consequat sunt.
+      Mollit aliqua est commodo exercitation fugiat elit adipisicing aliquip
+      dolore reprehenderit dolor exercitation qui.
+    </div>
+  );
+}
+export default Content;
